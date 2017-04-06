8681a28833d164dbe30a11794180e037b32ddd24
Pete Rossman <pete.rossman@gmail.com>
2017-04-06 11:42:49 -0400

    Add comments and move user prompt into tryInt method

--- PrimeFinder/Program.cs
+++ PrimeFinder/Program.cs
@@ -3,17 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+//Pete Rossman
+//April 6, 2017
 namespace PrimeFinder
 {
+    //This program asks the user which prime number he or she wants to find,
+    //and then outputs it.
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Which prime number are you looking for?");
-            int prime = tryInt(Console.ReadLine());
+            int prime = tryInt();
             int primeCounter = 0;
             int lastPrime = 2;
+
+            //This loop adds 1 to the primeCounter every time isPrime(i) is true,
+            //or in other words, a number is proven to be prime. The loop runs until
+            //the primeCounter equals the user inputted value.
+
             for (int i = 2; primeCounter < prime; i++)
             {
                 if (isPrime(i))
@@ -25,6 +32,10 @@ namespace PrimeFinder
             Console.WriteLine("The number " + prime + " prime is: " + lastPrime);
             Console.ReadLine();
         }
+
+        //isPrime runs through every possible factor of a number. If none divide evenly into
+        //the passed value, true is passed, in turn increasing the prime counter.
+        //If at any time a number divides evenly into the passed value, false is returned.
 
         public static bool isPrime(int i)
         {
@@ -38,16 +49,19 @@ namespace PrimeFinder
             return true;
         }
 
-        public static int tryInt(string number)
+        //tryInt confirms the user entered a valid input.
+
+        public static int tryInt()
         {
             bool runLoop = true;
             int myInt = 0;
             while (runLoop)
             {
                 runLoop = false;
+                Console.WriteLine("Which prime number are you looking for?");
                 try
                 {
-                    myInt = int.Parse(number);
+                    myInt = int.Parse(Console.ReadLine());
                     if (myInt < 1)
                     {
                         throw new IndexOutOfRangeException();
